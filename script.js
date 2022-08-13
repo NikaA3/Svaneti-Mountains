acccb031ab0a71fe46733a5e0e67b93e7f6cad56
NikaA3 <abuashvili.nika@yahoo.com>
2022-08-13 15:27:49 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) =>
+  panel.addEventListener("click", function () {
+    removeClassActive();
+    panel.classList.add("active");
+  })
+);
+
+function removeClassActive() {
+  panels.forEach((panel) => panel.classList.remove("active"));
+}
